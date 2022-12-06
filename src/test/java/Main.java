413dfa87232af029e56667fb1d2a1c7dca17f635
Cristi Vasile <ing_vasile_cristian@yahoo.co.uk>
2022-12-06 07:19:20 +0200

    added LoginTest

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,12 +1,19 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Wait;
 
 public class Main {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-
-        driver.manage().window().maximize();
-        driver.get("http://testfasttrackit.info/selenium-test/");
+    LoginTest login = new LoginTest();
+    login.loginWithValidData();
+    }
+    public static void wait(int seconds){
+        try{
+            Thread.sleep(seconds*1000L);
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
     }
 }
